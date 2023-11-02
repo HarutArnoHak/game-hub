b0cad00b78f72295c65dd40e6505a786a0a47e18
haruthak <hak0887@gmail.com>
2023-11-02 15:38:29 -0700

    show a spinner while fetching genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton, Card, CardBody, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height={"200px"}></Skeleton>
+      <Skeleton height={"200px"} width={"150px"}></Skeleton>
       <CardBody>
         <SkeletonText></SkeletonText>
       </CardBody>
